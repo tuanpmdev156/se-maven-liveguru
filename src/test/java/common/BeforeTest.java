cfc4c617047b8f4e0160ca78654b62cb07e48cda
tuanpmdev156 <tuanpmdev156@gmail.com>
2025-04-13 16:45:30 +0700

    Liveguru - Delete old files in allure-results

---
+++ src/test/java/common/BeforeTest.java
@@ -0,0 +1,20 @@
+package commons;
+
+import org.testng.annotations.BeforeSuite;
+
+import java.io.File;
+
+public class BeforeTest {
+    @BeforeSuite
+    public void cleanAllureResults() {
+        File folder = new File("allure-results");
+        if (folder.exists() && folder.isDirectory()) {
+            for (File file : folder.listFiles()) {
+                if (file.getName().endsWith(".json")) {
+                    file.delete();
+                }
+            }
+            System.out.println("Old Allure JSON files deleted.");
+        }
+    }
+}
